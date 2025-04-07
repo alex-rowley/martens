d315a17346b8110af96137867f0e1e4fd69ee541
Alex Rowley <arowley@gmail.com>
2025-04-07 11:54:13 +1000

    chore: Release 0.3.13

--- martens/__init__.py
+++ martens/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Alex Rowley'
 __email__ = ''
-__version__ = '0.3.12'
+__version__ = '0.3.13'
 
 from .martens import Dataset, SourceFile, stack, initialise, average
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/arowley-ai/martens',
-    version='0.3.12',
+    version='0.3.13',
     zip_safe=False
 )
