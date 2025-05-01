214345cb4adb6616484789515323ed88f5defcef
Alex Rowley <arowley@gmail.com>
2025-05-01 15:25:54 +1000

    {bug} Did not export SourceStream

--- martens/__init__.py
+++ martens/__init__.py
@@ -4,4 +4,4 @@ __author__ = 'Alex Rowley'
 __email__ = ''
 __version__ = '0.3.23'
 
-from .martens import Dataset, SourceFile, stack, initialise, average
+from .martens import Dataset, SourceFile, SourceStream, stack, initialise, average
