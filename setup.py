f3465240ece67f5e2954c4dbf98e78d32ba3011f
Alex Rowley <arowley@gmail.com>
2025-05-27 13:05:30 +1000

    chore: Release 0.4.1

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/arowley-ai/martens',
-    version='0.4.0',
+    version='0.4.1',
     zip_safe=False
 )
