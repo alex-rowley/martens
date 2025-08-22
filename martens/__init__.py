3ece677e2c4e52af388486f7fbbd97fb0fc8d92b
Alex Rowley <arowley@gmail.com>
2025-08-22 14:45:51 +1000

    chore: Release 0.4.23

--- martens/__init__.py
+++ martens/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Alex Rowley'
 __email__ = ''
-__version__ = '0.4.22'
+__version__ = '0.4.23'
 
 from .martens import Dataset, SourceFile, SourceStream, stack, initialise, average, median
